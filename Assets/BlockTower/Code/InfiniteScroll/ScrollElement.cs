a3a91659d79b27957178be7fb80cf2cc316cdca7
Artem Venger <artem.venger.professional@gmail.com>
2025-03-12 22:09:41 +0700

    Display number in scroll element

--- Assets/BlockTower/Code/InfiniteScroll/ScrollElement.cs
+++ Assets/BlockTower/Code/InfiniteScroll/ScrollElement.cs
@@ -1,15 +1,26 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace BlockTower
 {
     public class ScrollElement : MonoBehaviour
     {
+        [SerializeField]
+        private TMP_Text _numberField;
+
         public void SetData(ScrollElementData data)
         {
+            SetNumberFieldText(data.Number.ToString());
         }
 
         public void ResetData()
         {
+            SetNumberFieldText(string.Empty);
+        }
+
+        private void SetNumberFieldText(string text)
+        {
+            _numberField.text = text;
         }
     }
 }

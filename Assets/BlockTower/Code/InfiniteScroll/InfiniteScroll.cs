d96627b71c38bd98b7b8528b22b93a0d0373f514
Artem Venger <artem.venger.professional@gmail.com>
2025-03-12 21:27:28 +0700

    Create empty infinite scroll component

---
+++ Assets/BlockTower/Code/InfiniteScroll/InfiniteScroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BlockTower
+{
+    [RequireComponent(typeof(ScrollRect))]
+    public class InfiniteScroll : MonoBehaviour
+    {
+        private ScrollRect _scrollRect;
+        
+        private void Awake()
+        {
+            _scrollRect = GetComponent<ScrollRect>();
+        }
+    }
+}

8749fd354602d3813054f08c4f38177af87792de
Artem Venger <artem.venger.professional@gmail.com>
2025-03-13 17:49:08 +0700

    Add Anchored position property

--- Assets/BlockTower/Code/InfiniteScroll/ScrollElement.cs
+++ Assets/BlockTower/Code/InfiniteScroll/ScrollElement.cs
@@ -8,6 +8,19 @@ namespace BlockTower
         [SerializeField]
         private TMP_Text _numberField;
 
+        private RectTransform _transform;
+        
+        private void Awake()
+        {
+            _transform = (RectTransform)transform;
+        }
+
+        public float AnchoredPositionX
+        {
+            get => _transform.anchoredPosition.x;
+            set => _transform.anchoredPosition = new Vector2(value, _transform.anchoredPosition.y);
+        }
+
         public void SetData(ScrollElementData data)
         {
             SetNumberFieldText(data.Number.ToString());

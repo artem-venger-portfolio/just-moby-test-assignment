f1d2d15f194c02c0aad70fb497358e424bd67aec
Artem Venger <artem.venger.professional@gmail.com>
2025-03-14 21:28:05 +0700

    Detect dragging and draggable object

--- Assets/BlockTower/Code/Block.cs
+++ Assets/BlockTower/Code/Block.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BlockTower
 {
     public class Block : MonoBehaviour
     {
+        [SerializeField]
+        private Image _image;
+
+        public void SetDraggingColor()
+        {
+            _image.color = Color.green;
+        }
+
+        public void ResetColor()
+        {
+            _image.color = Color.white;
+        }
+
+        private void LogInfo(string message)
+        {
+            Debug.Log($"[{name}] {message}");
+        }
     }
 }
--- Assets/BlockTower/Code/Scroll.cs
+++ Assets/BlockTower/Code/Scroll.cs
@@ -5,21 +5,49 @@ namespace BlockTower
 {
     public class Scroll : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
     {
-        [SerializeField]
-        private Block[] _blocks;
+        private Block _draggingBlock;
+        private bool _isDragging;
 
         public void OnBeginDrag(PointerEventData eventData)
         {
-            LogInfo(nameof(OnBeginDrag));
+            var isDragMove = eventData.delta.y > 0;
+            if (isDragMove == false)
+            {
+                return;
+            }
+
+            var raycastTarget = eventData.pointerPressRaycast.gameObject;
+            var block = raycastTarget.GetComponentInParent<Block>();
+            if (block == null)
+            {
+                return;
+            }
+
+            _draggingBlock = block;
+            _draggingBlock.SetDraggingColor();
+            _isDragging = true;
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
-            LogInfo(nameof(OnEndDrag));
+            if (_isDragging == false)
+            {
+                return;
+            }
+
+            _draggingBlock.ResetColor();
+
+            _isDragging = false;
+            _draggingBlock = null;
+            LogInfo(message: "Drag end");
         }
 
         public void OnDrag(PointerEventData eventData)
         {
+            if (_isDragging == false)
+            {
+                return;
+            }
         }
 
         private void LogInfo(string message)

504514acaf4d92ede49aeece561c377b241fa508
Artem Venger <artem.venger.professional@gmail.com>
2025-03-14 20:14:12 +0700

    Create scroll element

---
+++ Assets/BlockTower/Code/Scroll.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BlockTower
+{
+    public class Scroll : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+    {
+        [SerializeField]
+        private Block[] _blocks;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            LogInfo(nameof(OnBeginDrag));
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            LogInfo(nameof(OnEndDrag));
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+        }
+
+        private void LogInfo(string message)
+        {
+            Debug.Log($"[{nameof(name)}] {message}");
+        }
+    }
+}

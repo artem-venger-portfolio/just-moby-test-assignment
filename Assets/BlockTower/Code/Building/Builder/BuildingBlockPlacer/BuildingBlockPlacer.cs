a01a36e113e73e310be553f0d288de919de37ebf
Artem Venger <artem.venger.professional@gmail.com>
2025-03-15 18:27:09 +0700

    Place new block above last block

--- Assets/BlockTower/Code/Building/Builder/BuildingBlockPlacer/BuildingBlockPlacer.cs
+++ Assets/BlockTower/Code/Building/Builder/BuildingBlockPlacer/BuildingBlockPlacer.cs
@@ -1,4 +1,6 @@
-﻿namespace BlockTower.Building
+﻿using UnityEngine;
+
+namespace BlockTower.Building
 {
     public class BuildingBlockPlacer : IBuildingBlockPlacer
     {
@@ -9,9 +11,34 @@
             _tower = tower;
         }
 
-        public void Place(BlockBase block)
+        public void Place(BlockBase placingBlock)
         {
-            _tower.Add(block);
+            if (_tower.IsEmpty() == false)
+            {
+                placingBlock.Transform.position = GetPositionAboveLastBlock(placingBlock);
+            }
+
+            _tower.Add(placingBlock);
+        }
+
+        private Vector3 GetPositionAboveLastBlock(BlockBase placingBlock)
+        {
+            var lastBlock = _tower.GetLastBlock();
+            var lastBlockTransform = lastBlock.Transform;
+            var lastBlockPosition = lastBlockTransform.position;
+            var lastBlockDistanceToTop = lastBlockTransform.rect.yMax;
+            var lastBlockTopY = lastBlockPosition.y + lastBlockDistanceToTop;
+
+            var placingBlockTransform = placingBlock.Transform;
+            var placingBlockPosition = placingBlockTransform.position;
+            var placingBlockDistanceToBottom = placingBlockTransform.rect.xMin;
+
+            var targetX = placingBlockPosition.x;
+            var targetY = lastBlockTopY - placingBlockDistanceToBottom;
+            var targetZ = placingBlockPosition.z;
+            var targetPosition = new Vector3(targetX, targetY, targetZ);
+
+            return targetPosition;
         }
     }
 }

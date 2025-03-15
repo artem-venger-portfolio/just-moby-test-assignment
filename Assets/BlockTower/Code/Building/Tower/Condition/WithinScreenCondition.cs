473926ed63f55c1f1ee629e92bcb0c425494717a
Artem Venger <artem.venger.professional@gmail.com>
2025-03-15 17:16:37 +0700

    Created within screen condition

---
+++ Assets/BlockTower/Code/Building/Tower/Condition/WithinScreenCondition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace BlockTower.Building
+{
+    public class WithinScreenCondition : ICondition
+    {
+        public bool Check(BlockBase block)
+        {
+            var corners = block.GetWorldCorners();
+            var bottomLeft = corners[0];
+            var topRight = corners[2];
+
+            const int min_x = 0;
+            var maxX = Screen.width;
+            const int min_y = 0;
+            var maxY = Screen.height;
+
+            return bottomLeft.x >= min_x &&
+                   bottomLeft.y >= min_y &&
+                   topRight.x <= maxX &&
+                   topRight.y <= maxY;
+        }
+    }
+}

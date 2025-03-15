740e5fbabf5fd717b7d0b45a2080e82980bec5e8
Artem Venger <artem.venger.professional@gmail.com>
2025-03-15 18:12:16 +0700

    Place one object above another

---
+++ Assets/BlockTower/Code/PlaceAbove/PlaceAbove.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace BlockTower.PlaceAbove
+{
+    public class PlaceAbove : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform _object;
+
+        [SerializeField]
+        private RectTransform _object2;
+
+        [SerializeField]
+        private TMP_Text _textFiled;
+
+        private void Start()
+        {
+            var currentPosition = _object.position;
+            var bottomOffset = _object.rect.yMin;
+
+            var targetPosition = _object2.position;
+            var targetTopOffset = _object2.rect.yMax;
+            var targetTopY = targetPosition.y + targetTopOffset;
+
+            var newPositionY = targetTopY - bottomOffset;
+
+            _object.position = new Vector3(currentPosition.x, newPositionY, currentPosition.z);
+        }
+
+        private void Update()
+        {
+            _textFiled.text = $"Min.x {_object.rect.min.x} xMin {_object.rect.xMin}";
+        }
+    }
+}

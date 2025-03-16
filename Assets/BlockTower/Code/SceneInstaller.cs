d2f32a5fa5193e0a7249edb52e2509eca25ece9b
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 13:33:33 +0700

    Clear installer

--- Assets/BlockTower/Code/SceneInstaller.cs
+++ Assets/BlockTower/Code/SceneInstaller.cs
@@ -1,19 +1,11 @@
-﻿using UnityEngine;
-using Zenject;
+﻿using Zenject;
 
 namespace BlockTower
 {
     public class SceneInstaller : MonoInstaller<SceneInstaller>
     {
-        [SerializeField]
-        private ScrollBase _scroll;
-
         public override void InstallBindings()
         {
-            Container.Bind<ScrollBase>()
-                     .FromInstance(_scroll)
-                     .AsSingle()
-                     .NonLazy();
         }
     }
 }

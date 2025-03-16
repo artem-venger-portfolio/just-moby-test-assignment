45ffc976fd669535e8e624d346cc01cc3e13402d
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 20:44:08 +0700

    Add logic to Tower

--- Assets/BlockTower/Code/Tower/ITower.cs
+++ Assets/BlockTower/Code/Tower/ITower.cs
@@ -2,5 +2,11 @@
 {
     public interface ITower
     {
+        bool IsEmpty();
+        TowerBlockBase GetLastBlock();
+        void Add(TowerBlockBase block);
+        void Remove(TowerBlockBase block);
+
+        TowerBlockBase this[int i] { get; }
     }
 }
--- Assets/BlockTower/Code/Tower/Tower.cs
+++ Assets/BlockTower/Code/Tower/Tower.cs
@@ -1,9 +1,33 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace BlockTower
 {
     [UsedImplicitly]
     public class Tower : ITower
     {
+        private readonly List<TowerBlockBase> _blocks = new();
+
+        public bool IsEmpty()
+        {
+            return _blocks.Count == 0;
+        }
+
+        public TowerBlockBase GetLastBlock()
+        {
+            return _blocks[^1];
+        }
+
+        public void Add(TowerBlockBase block)
+        {
+            _blocks.Add(block);
+        }
+
+        public void Remove(TowerBlockBase block)
+        {
+            _blocks.Remove(block);
+        }
+
+        public TowerBlockBase this[int i] => _blocks[i];
     }
 }

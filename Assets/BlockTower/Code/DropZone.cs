d709354188f4a6517c481a970bae20316099010a
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 19:38:01 +0700

    Create DropZone

---
+++ Assets/BlockTower/Code/DropZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace BlockTower
+{
+    public class DropZone : MonoBehaviour
+    {
+        private UIUtility _uiUtility;
+
+        public bool IsInZone(Vector2 screenPoint)
+        {
+            return _uiUtility.RectangleContainsScreenPoint((RectTransform)transform, screenPoint);
+        }
+
+        [Inject]
+        private void InjectDependencies(UIUtility uiUtility)
+        {
+            _uiUtility = uiUtility;
+        }
+    }
+}

5ccd8957c607737cfc3433227c52aea0a473436d
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 22:14:21 +0700

    Change destruction animations

--- Assets/BlockTower/Code/Tower/Builder/DestructionAnimator/DestructionAnimator.cs
+++ Assets/BlockTower/Code/Tower/Builder/DestructionAnimator/DestructionAnimator.cs
@@ -15,8 +15,8 @@ namespace BlockTower
             _animations = new Func<RectTransform, Image, Tween>[]
             {
                 ScaleAndFadeDestruction,
-                MoveOffScreenDestruction,
-                ExplodeDestruction,
+                MoveToNewPosition,
+                SubtleDestruction,
             };
         }
 
@@ -34,20 +34,20 @@ namespace BlockTower
             return sequence;
         }
 
-        private Tween MoveOffScreenDestruction(RectTransform transform, Image image)
+        private Tween MoveToNewPosition(RectTransform transform, Image image)
         {
-            return transform.DOMoveX(endValue: -1000f, duration: 1f).SetEase(Ease.InOutCubic);
+            var randomX = Random.Range(-Screen.width / 2f, Screen.width / 2f);
+            var randomY = Random.Range(-Screen.height / 2f, Screen.height / 2f);
+            return transform.DOMove(new Vector3(randomX, randomY, z: 0), duration: 1f).SetEase(Ease.InOutSine);
         }
 
-        private Tween ExplodeDestruction(RectTransform transform, Image image)
+        private Tween SubtleDestruction(RectTransform transform, Image image)
         {
             var sequence = DOTween.Sequence();
-            sequence.Append(transform.DOScale(Vector3.zero, duration: 0.5f).SetEase(Ease.InQuad));
-            sequence.Join(image.DOFade(endValue: 0f, duration: 0.5f).SetEase(Ease.InQuad));
-            sequence.Join(transform
-                         .DOMove(new Vector3(Random.Range(minInclusive: -200, maxExclusive: 200), Random.Range(minInclusive: -200, maxExclusive: 200), z: 0),
-                                 duration: 0.5f)
-                         .SetEase(Ease.OutBounce));
+            sequence.Append(transform.DOScale(Vector3.zero, duration: 0.8f).SetEase(Ease.InQuad));
+            sequence.Join(image.DOFade(endValue: 0f, duration: 0.8f).SetEase(Ease.InQuad));
+            sequence.Join(transform.DORotate(new Vector3(x: 0, y: 0, Random.Range(minInclusive: -45f, maxInclusive: 45f)),
+                                             duration: 0.8f, RotateMode.FastBeyond360));
             return sequence;
         }
     }

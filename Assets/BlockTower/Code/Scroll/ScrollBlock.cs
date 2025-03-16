276fb2aaa80ca34a5cbc45b9707d79f609fb1d3c
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 18:37:22 +0700

    Check that block at pointer position and add drag methods

--- Assets/BlockTower/Code/Scroll/ScrollBlock.cs
+++ Assets/BlockTower/Code/Scroll/ScrollBlock.cs
@@ -10,9 +10,42 @@ namespace BlockTower
         [SerializeField]
         private Image _image;
 
+        [SerializeField]
+        private DraggableObject _draggableObject;
+
+        private UIUtility _uiUtility;
+
         public void SetColor(Color color)
         {
             _image.color = color;
+        }
+
+        public bool IsAtScreenPoint(Vector2 screenPoint)
+        {
+            return _uiUtility.RectangleContainsScreenPoint(Transform, screenPoint);
+        }
+
+        public void OnBeginDrag()
+        {
+            _draggableObject.OnBeginDrag();
+        }
+
+        public void OnDrag(Vector2 position)
+        {
+            _draggableObject.OnDrag(position);
+        }
+
+        public void OnEndDrag()
+        {
+            _draggableObject.OnEndDrag();
+        }
+
+        private RectTransform Transform => (RectTransform)transform;
+
+        [Inject]
+        private void InjectDependencies(UIUtility uiUtility)
+        {
+            _uiUtility = uiUtility;
         }
 
         [UsedImplicitly]

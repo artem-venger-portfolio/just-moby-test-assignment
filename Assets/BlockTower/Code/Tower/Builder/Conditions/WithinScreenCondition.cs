ae45c12b18717aad1c48117a76a11fd9559da0e5
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 20:49:53 +0700

    Create WithinScreenCondition condition

---
+++ Assets/BlockTower/Code/Tower/Builder/Conditions/WithinScreenCondition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace BlockTower
+{
+    public class WithinScreenCondition : IBuildCondition
+    {
+        public bool CanBuild(BuildConditionData data)
+        {
+            var corners = data.CheckingBlockCorners;
+            var bottomLeft = corners[0];
+            var topRight = corners[2];
+
+            const int min_x = 0;
+            var maxX = Screen.width;
+            const int min_y = 0;
+            var maxY = Screen.height;
+
+            return bottomLeft.x >= min_x &&
+                   bottomLeft.y >= min_y &&
+                   topRight.x <= maxX;
+        }
+    }
+}

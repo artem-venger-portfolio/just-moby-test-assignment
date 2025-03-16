552dfffd37e9c6591a17412418816b88f03ea33d
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 17:40:16 +0700

    Instantiate ScrollBlocks

--- Assets/BlockTower/Code/Scroll/Scroll.cs
+++ Assets/BlockTower/Code/Scroll/Scroll.cs
@@ -6,17 +6,29 @@ namespace BlockTower
     {
         private IGameConfig _config;
         private IProjectLogger _logger;
+        private ScrollBlock.Factory _blockFactory;
+        private ScrollBlock[] _blocks;
 
         public override void CreateBlocks()
         {
             LogInfo(nameof(CreateBlocks));
+            var colors = _config.Colors;
+            _blocks = new ScrollBlock[colors.Count];
+            for (var i = 0; i < colors.Count; i++)
+            {
+                var currentColor = colors[i];
+                var currentBlock = _blockFactory.Create();
+                currentBlock.SetColor(currentColor);
+                _blocks[i] = currentBlock;
+            }
         }
 
         [Inject]
-        private void InjectDependencies(IGameConfig config, IProjectLogger logger)
+        private void InjectDependencies(IGameConfig config, IProjectLogger logger, ScrollBlock.Factory blockFactory)
         {
             _config = config;
             _logger = logger;
+            _blockFactory = blockFactory;
         }
 
         private void LogInfo(string message)

7510b16c8322538965d105f3e978e56fea3f3c42
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 17:31:43 +0700

    Create ScrollBlock

---
+++ Assets/BlockTower/Code/Scroll/ScrollBlock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace BlockTower
+{
+    public class ScrollBlock : MonoBehaviour
+    {
+        [SerializeField]
+        private Image _image;
+
+        public void SetColor(Color color)
+        {
+            _image.color = color;
+        }
+        
+        public class Factory : PlaceholderFactory<ScrollBlock, Color>
+        {
+        }
+    }
+}

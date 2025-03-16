bf2c9c6a1cb25791000c4b01fd89a2824e63c2d7
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 18:08:54 +0700

    Add ScrollBlock drag logic

--- Assets/BlockTower/Code/Scroll/Scroll.cs
+++ Assets/BlockTower/Code/Scroll/Scroll.cs
@@ -1,12 +1,23 @@
-﻿using Zenject;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Zenject;
 
 namespace BlockTower
 {
-    public class Scroll : ScrollBase
+    public class Scroll : ScrollBase, IBeginDragHandler, IEndDragHandler, IDragHandler
     {
+        [SerializeField]
+        private Transform _draggingObjectContainer;
+
+        [SerializeField]
+        private ScrollRect _scrollRect;
+
         private IGameConfig _config;
         private IProjectLogger _logger;
         private ScrollBlock.Factory _blockFactory;
+        private DraggableObject _draggingObject;
+        private bool _isDragging;
         private ScrollBlock[] _blocks;
 
         public override void CreateBlocks()
@@ -29,6 +40,56 @@ namespace BlockTower
             _config = config;
             _logger = logger;
             _blockFactory = blockFactory;
+        }
+
+        void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+        {
+            var isDragMove = eventData.delta.y > 0;
+            if (isDragMove == false)
+            {
+                return;
+            }
+
+            var raycastTarget = eventData.pointerPressRaycast.gameObject;
+            var draggingObject = raycastTarget.GetComponentInParent<DraggableObject>();
+            if (draggingObject == null)
+            {
+                return;
+            }
+
+            _draggingObject = draggingObject;
+            _draggingObject.SetDraggingObjectContainer(_draggingObjectContainer);
+            _draggingObject.OnBeginDrag();
+            SetIsDraggingAndChangeScrollActivity(isDragging: true);
+        }
+
+        void IDragHandler.OnDrag(PointerEventData eventData)
+        {
+            if (_isDragging == false)
+            {
+                return;
+            }
+
+            _draggingObject.OnDrag(eventData.position);
+        }
+
+        void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+        {
+            if (_isDragging == false)
+            {
+                return;
+            }
+
+            _draggingObject.OnEndDrag();
+            _draggingObject = null;
+
+            SetIsDraggingAndChangeScrollActivity(isDragging: false);
+        }
+
+        private void SetIsDraggingAndChangeScrollActivity(bool isDragging)
+        {
+            _isDragging = isDragging;
+            _scrollRect.horizontal = !_isDragging;
         }
 
         private void LogInfo(string message)

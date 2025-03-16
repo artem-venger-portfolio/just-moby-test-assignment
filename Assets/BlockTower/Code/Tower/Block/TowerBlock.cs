c341bb2bd05c2a038fc7884d34378ae8c0f76f51
Artem Venger <artem.venger.professional@gmail.com>
2025-03-16 20:55:28 +0700

    Move logic to realization

--- Assets/BlockTower/Code/Tower/Block/TowerBlock.cs
+++ Assets/BlockTower/Code/Tower/Block/TowerBlock.cs
@@ -1,9 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace BlockTower
 {
     public class TowerBlock : TowerBlockBase
     {
+        [SerializeField]
+        private Image _image;
+
+        [SerializeField]
+        private DraggableObject _draggableObject;
+
+        public override Color Color
+        {
+            get => _image.color;
+            set => _image.color = value;
+        }
+
         public override Vector3[] GetWorldCorners()
         {
             var corners = new Vector3[4];
--- Assets/BlockTower/Code/Tower/Block/TowerBlockBase.cs
+++ Assets/BlockTower/Code/Tower/Block/TowerBlockBase.cs
@@ -1,22 +1,10 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 namespace BlockTower
 {
     public abstract class TowerBlockBase : MonoBehaviour
     {
-        [SerializeField]
-        private Image _image;
-
-        [SerializeField]
-        private DraggableObject _draggableObject;
-
-        public Color Color
-        {
-            get => _image.color;
-            set => _image.color = value;
-        }
-
+        public abstract Color Color { get; set; }
         public abstract Vector3[] GetWorldCorners();
     }
 }

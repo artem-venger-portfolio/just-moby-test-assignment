942cfe7fc05aef8aa64f9b628789823e2dfd7550
Artem Venger <artem.venger.professional@gmail.com>
2025-03-17 23:16:04 +0700

    Take placement animation parameters from config

--- Assets/BlockTower/Code/Tower/Builder/PlacementAnimator/PlacementAnimator.cs
+++ Assets/BlockTower/Code/Tower/Builder/PlacementAnimator/PlacementAnimator.cs
@@ -7,27 +7,32 @@ namespace BlockTower
     [UsedImplicitly]
     public class PlacementAnimator : IPlacementAnimator
     {
+        private readonly PlacementAnimationConfig _config;
+
+        public PlacementAnimator(IGameConfig gameConfig)
+        {
+            _config = gameConfig.PlacementAnimationConfig;
+        }
+
         public Tween StartAnimation(RectTransform transform, Vector3 targetPositoin)
         {
             var sequence = DOTween.Sequence();
 
             var currentPosition = transform.position;
             var spinPosition = new Vector3(targetPositoin.x, currentPosition.y, targetPositoin.z);
-            const float move_to_spin_position_speed = 1f;
             var moveToSpinPositionDuration = Vector3.Distance(currentPosition, spinPosition) /
-                                             move_to_spin_position_speed;
+                                             _config.MoveToSpinPositionSpeed;
             var moveToSpinPositionAnimation = transform.DOMove(spinPosition, moveToSpinPositionDuration)
                                                        .SetEase(Ease.Linear);
             sequence.Append(moveToSpinPositionAnimation);
 
-            const float placement_duration = 1f;
-            var moveToTargetPositionAnimation = transform.DOMove(targetPositoin, placement_duration)
+            var placementDuration = _config.PlacementDuration;
+            var moveToTargetPositionAnimation = transform.DOMove(targetPositoin, placementDuration)
                                                          .SetEase(Ease.InBack);
             sequence.Append(moveToTargetPositionAnimation);
 
             var spinAngle = new Vector3(x: 0, y: 0, z: 360);
-            const float spin_relative_duration = 0.7f;
-            var spinDuration = placement_duration * spin_relative_duration;
+            var spinDuration = placementDuration * _config.SpinRelativeDuration;
             var spinAnimation = transform.DORotate(spinAngle, spinDuration, RotateMode.FastBeyond360)
                                          .SetEase(Ease.OutCubic);
             sequence.Join(spinAnimation);

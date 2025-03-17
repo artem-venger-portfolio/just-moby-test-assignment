6ceeec0343754b10bb0d40a69f3b15f6d86b1069
Artem Venger <artem.venger.professional@gmail.com>
2025-03-17 18:23:23 +0700

    Subscribe block addition event

--- Assets/BlockTower/Code/Tower/Demolisher/TowerDemolisher.cs
+++ Assets/BlockTower/Code/Tower/Demolisher/TowerDemolisher.cs
@@ -1,22 +1,33 @@
 ﻿using JetBrains.Annotations;
+using R3;
 
 namespace BlockTower
 {
     [UsedImplicitly]
     public class TowerDemolisher : ITowerDemolisher
     {
+        private readonly CompositeDisposable _compositeDisposable;
         private readonly ITower _tower;
 
         public TowerDemolisher(ITower tower)
         {
             _tower = tower;
+            _compositeDisposable = new CompositeDisposable();
         }
 
         public void Start()
         {
+            _tower.BlockAdded
+                  .Subscribe(BlockAddedEventHandler)
+                  .AddTo(_compositeDisposable);
         }
 
         public void Stop()
+        {
+            _compositeDisposable.Clear();
+        }
+
+        private void BlockAddedEventHandler(TowerBlockBase block)
         {
         }
     }
